c10a8467e1d8b1ca0ff77e97dc3468489272aa53
dajuma <dajuma1995@gmail.com>
2023-11-21 13:23:02 -0800

    element pointers

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,5 +17,5 @@ struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-
+size_t listint_len(const listint_t *h);
 #endif
